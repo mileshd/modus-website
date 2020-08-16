b539f786860bb9f63afcae6a076a4cb687f82dc6
Miles Dickinson <miles.h.dickinson@gmail.com>
2020-08-16 16:16:18 -0400

    Implement refactored MatMaturity Model

--- src/pages/maturity-model.js
+++ src/pages/maturity-model.js
@@ -19,33 +19,61 @@ const MaturityModelPage = ({ data, location }) => {
             This maturity model represents the stages that growing brands have been observed to undergo as they build out and expand their planning and executional processes. 
           </p>
 
-          <h2>Level 1: Just Starting Out</h2>
+          <h2>Level 1: Launch</h2>
           <h3>Systems:</h3>
-          <p>Separate spreadsheets, trade spend, promotions, and operational volume all planned separately.</p>
+          <p>Spreadsheets.</p>
+          <p>Trade spend, promotions, and operational volume are all planned separately.</p>
 
           <h3>Demand Planning:</h3>
           <p>
-            Planning horizon does not exceed 6 months. New distribution is marginally planned for. Customer orders are not analyzed vs. forecast. Customer behaviors and timings are not considered in the forecast.
+            Planning horizon is roughly six months.
+          </p>
+          <p>
+            New distribution is marginally planned for.
+          </p>
+          <p>
+            Customer orders are not analyzed vs. forecast.
+          </p>
+          <p>
+            Customer behaviors and distribution timing are not considered in the forecast.
           </p>
  
           <h3>Supply Planning:</h3>
           <p>
-            Production orders are planned from a separate spreadsheet. Non-finished goods vendors get a version of the latest demand planning spreadsheet. No long-term capacity planning is done.  
+            Production orders are planned from a separate spreadsheet.
+          </p>
+          <p>
+            Non-finished goods vendors are informed of the latest demand plan.
+          </p>
+          <p>
+            No long-term capacity planning.
           </p>
 
           <h3>Promotion Calendar:</h3>
           <p>
-            Promotion planning is ad hoc. No volume projections by promotion, no follow through to ensure promotion is executed. Trade spend by promotion is not tracked. No post promotional analysis is done.
+            Promotion planning is not systematic and is planned on a one-off basis.
+          </p>
+          <p>
+            No volume projections by promotion and limited follow through to ensure promotion is executed.
+          </p>
+          <p>
+            Trade spend by promotion is not tracked.
+          </p>
+          <p>
+            No post promotional analysis.
           </p>
 
           <h3>Trade Management:</h3>
           <p>
-            Trade rates by retailer are not persistently tracked or budgeted. Retailer margins are not projected. 
+            Trade rates by retailer are not persistently tracked or budgeted for. 
+          </p>
+          <p>
+            Retailer margins are largely ignored.
           </p>
 
           <h3>Trade Deductions:</h3>
           <p>
-            Deductions are not validated vs. Forecasted spend
+            Retailer or distributor deductions are not validated vs. forecasted spend.
           </p>
 
           <h3>Baseline Management:</h3>
@@ -63,7 +91,7 @@ const MaturityModelPage = ({ data, location }) => {
             Management team has many unknown unknowns regarding the planning process.
           </p>
 
-          <h2>Level 2: Building the Business</h2>
+          <h2>Level 2: Early Stage Growth</h2>
           <h3>Systems:</h3>
           <p>
             Collaborative spreadsheets or spreadsheet-like tools either live in the cloud or in a shared location.
@@ -71,32 +99,68 @@ const MaturityModelPage = ({ data, location }) => {
 
           <h3>Demand Planning:</h3>
           <p>
-            Planning horizon of a year to 18mos. New distribution is planned for at the product group level, but SKU mix and other details are not. Customer orders are analyzed vs. Forecast in aggregate. Consumption to delivery Forecast takes shape as promotion volume offsets are implemented.
+            Planning horizon of a year to 18 months.
+          </p>
+          <p>
+            New distribution is planned for at the product group level, but SKU mix and other details are not. 
+          </p>
+          <p>
+            Customer orders are analyzed vs. the forecast in aggregate. 
+          </p>
+          <p>
+            Consumption to delivery forecast is slowly developed as promotion volume offsets are implemented.
           </p>
  
           <h3>Supply Planning:</h3>
           <p>
-            Long term capacity plans are created from the longer planning horizon, which are fed to non-finished goods suppliers and production. Production plans are maintained on a collaborative spreadsheet.
+            Long term capacity plans are created from the longer planning horizon, which are provided to non-finished goods suppliers and production teams. 
+          </p>
+          <p>
+            Production plans are maintained on a collaborative spreadsheet.
           </p>
 
           <h3>Promotion Calendar:</h3>
           <p>
-            Promotion planning is centralized. Volume projections by promotion exist. Customer service identifies if orders placed conform to expected volume. Trade spend by promotion is tracked, but not retailer margin. Top-line post promotional analysis is done comparing quantities scanned vs. Expected consumption. 
+            Promotion planning is centralized across departments. 
+          </p>
+          <p>
+            Volume projections by promotion are instituted.
+          </p>
+          <p>
+            Customer service identifies if orders conform to expected volume.
+          </p>
+          <p>
+            Trade spend by promotion is typically tracked, but retailer margin is still largely ignored.
+          </p>
+          <p>
+            Top-line post promotional analysis is tracked comparing quantities scanned vs. expected consumption.
           </p>
 
           <h3>Trade Management:</h3>
           <p>
-            Trade rates by retailer are persistently tracked vs. a budget. All types of promotions including ads, TPRs, and coupons are tracked. Retailer margins are projected. 
+            Trade rates by retailer are persistently tracked vs. a budget.
+          </p>
+          <p>
+            All types of promotions, including advertisements, temporary price reductions and coupons are tracked. 
+          </p>
+          <p>
+            Retailer margins are projected.
           </p>
 
           <h3>Trade Deductions:</h3>
           <p>
-            Deductions are validated as expected or not, but not detailed vs. Forecasted spend.
+            Typically, deductions are validated.
+          </p>
+          <p>
+            Actual vs. forecasted spend is still often overlooked.
           </p>
 
           <h3>Baseline Management:</h3>
           <p>
-            Baseline volumes are reviewed at regular intervals and maintained at the SKU level. Baseline development is overwritten when actualized. 
+            Baseline volumes are reviewed at regular intervals and maintained at the SKU level.
+          </p>
+          <p>
+            Realized baselines inform the forecast going forward.
           </p>
 
           <h3>Cross-Functional Process:</h3>
@@ -106,10 +170,10 @@ const MaturityModelPage = ({ data, location }) => {
 
           <h3>Management:</h3>
           <p>
-            Management team knows the unknowns they have regarding planning
+            Management team is beginning to track variables that they previously ignored.
           </p>
 
-          <h2>Level 3: Modus Current State</h2>
+          <h2>Level 3: Late Stage Growth - Current Modus State</h2>
           <h3>Systems:</h3>
           <p>
             Centralized, integrated, structured, and collaborative platform purpose built for bottoms-up planning employed. (Modus Planning) 
@@ -117,42 +181,75 @@ const MaturityModelPage = ({ data, location }) => {
 
           <h3>Demand Planning:</h3>
           <p>
-            Planning horizon unlimited in potential scope. New distribution is planned for by SKU, store count, and timing. Customer orders are analyzed vs. forecast by line item. Consumption to delivery forecast is refined as promotion volume offsets are implemented at the DC level.
+            Planning horizon is theoretically unlimited, however typically analyzed a year to two years in advance. 
+          </p>
+          <p>
+            New distribution is planned for by SKU, store count, and distribution timing. 
+          </p>
+          <p>
+            Customer orders are analyzed vs. forecast by line item. 
+          </p>
+          <p>
+            Consumption to delivery forecast is refined as promotion volume offsets are implemented at the distribution center level.
           </p>
  
           <h3>Supply Planning:</h3>
           <p>
-            Long term capacity planning is accomplished with production planning done on the weekly level is enabled, lowering inventory holding costs. 
+            Long term capacity planning is accomplished with production planning.
+          </p>
+          <p>
+            Production planning is reviewed weekly, which lowers inventory holding costs.
           </p>
 
           <h3>Promotion Calendar:</h3>
           <p>
-            Promotion planning is centralized and real-time on one system. Volume projections by promotion by retailer. Customer service identifies if orders placed conform to expected volume on the SKU level - looking at expected customer days on hand. Trade spend and retailer margin by promotion is tracked. Top-line post promotional analysis is done comparing quantities scanned vs. expected consumption. 
+            Real-time, centralized promotion planning.
+          </p>
+          <p>
+            Volume projections by promotion by retailer are reviewed. 
+          </p>
+          <p>
+            Customer service identifies if orders conform to expected volume on the SKU level by reviewing expected customer days on hand. 
+          </p>
+          <p>
+            Trade spend and retailer margin by promotion is tracked. 
+          </p>
+          <p>
+            Top-line post promotional analysis is completed by comparing quantities scanned vs. expected consumption.
           </p>
 
           <h3>Trade Management:</h3>
           <p>
-            Trade rates by retailer are persistently tracked vs. a budget. All types of promotions including ads, TPRs, and coupons are tracked. 
+            Trade rates by retailer are persistently tracked vs. a budget. 
+          </p>
+          <p>
+            All types of promotions including advertisements, temporary price reductions and coupons are tracked.
           </p>
 
           <h3>Trade Deductions:</h3>
           <p>
-            Deductions are validated at the dollar amount vs. spend calculated from actual scan data.
+            Deductions are validated from actual scan data.
           </p>
 
           <h3>Baseline Management:</h3>
           <p>
-            Baseline volumes are reviewed at regular intervals and maintained at the SKU/DC level. Baseline development is persistently tracked and actualized.
+            Baseline volumes are reviewed regularly and maintained at the SKU/DC level. 
+          </p>
+          <p>
+            Baseline development is persistently tracked and actualized.
           </p>
 
           <h3>Cross-Functional Process:</h3>
           <p>
-            All cross-functional members are refining their part in the forecast continuously as learned. Regularly scheduled S&OP reviews cover exceptions and trends. 
+            All cross-functional members contribute to the forecast continuously to ensure accuracy. 
+          </p>
+          <p>
+            Regularly scheduled S&OP reviews cover exceptions and trends.
           </p>
 
           <h3>Management:</h3>
           <p>
-            Management team has an action plan to address gaps knows the unknowns they have regarding planning.
+            Management team is better equipped to identify and address variables regarding go-to-market execution activity.  
           </p>
         </div>
       </div>
