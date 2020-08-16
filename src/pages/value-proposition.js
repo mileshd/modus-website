cb9ff1158955386561c91ef767c467ff3ca10949
Miles Dickinson <miles.h.dickinson@gmail.com>
2020-08-16 16:25:21 -0400

    Make changes to Modus Value Prop resource

--- src/pages/value-proposition.js
+++ src/pages/value-proposition.js
@@ -20,7 +20,7 @@ const ValuePropositionPage = ({ data, location }) => {
             Modus Planning has been proven to widen your margins by enabling its users to master go-to-market execution.
           </p>
 
-          <h2>1. Align Your Company</h2>
+          <h2>1. Align your company.</h2>
           <p>
             Modus Planning enables interdepartmental collaboration. Operations, supply chain, marketing, and finance operating from the same source of truth.
           </p>
@@ -39,7 +39,7 @@ const ValuePropositionPage = ({ data, location }) => {
           </p>
 
           <h2>
-            4. Deeply understand how your business’ performance is evolving.
+            4. Track your performance.
           </h2>
           <p>
             Modus Planning compares your shipments and scan data vs. the forecast by consignee, so you can consistently track over or under performing customers or products. 
@@ -48,45 +48,45 @@ const ValuePropositionPage = ({ data, location }) => {
             Track gross and net sales to understand margins and slotting investment ROIs.
           </p>
 
-          <h2>5. Never Stop Evolving.</h2>
+          <h2>5. Never stop evolving.</h2>
           <p>
-            Much of the business planning within CPG is educated guesswork. With Modus Planning, all relevant data is tracked to enable repeat success and knowledge-gaining from mistakes, meaning forecasts, lift percentages, and customer orders all improve the longer a brand uses Modus. 
+            Much of the business planning within CPG is educated guesswork. With Modus, all relevant data is available to enable the user to repeat successes and learn from prior mistakes. Forecasts, lift percentages, and customer orders all improve the longer a brand uses Modus.
           </p>
           <p>
-            As you evolve Modus Planning is too, always incorporating best practices and new helpful features along the way.
+            Modus Planning is evolving as well, as feature updates and enhancements are regularly implemented. 
           </p>
 
-          <h2>6. Forecast Accurately.</h2>
+          <h2>6. Forecast accurately.</h2>
           <p>
             Accurate demand forecasts inform the production plan, can help project future growth, and assist with company-wide strategic decision making. Modus Planning has been proven to significantly improve forecast accuracy for its users.
           </p>
 
-          <h2>7. Eliminate Waste.</h2>
+          <h2>7. Eliminate waste.</h2>
           <p>
-            Brands are billed by retailers for spoilage or “reclaim.” Accurate forecasts eliminate this costly expense, which means more money in to drive sales, and importantly, less wasted food.
+            Brands are billed by retailers for spoilage or “reclaim”. Accurate forecasts eliminate this costly expense, which means wider margins and less wasted food.
           </p>
 
-          <h2>8. Avoid Missed Upside.</h2>
+          <h2>8. Avoid missed upside.</h2>
           <p>
-            Accurate forecasts ensure there is enough product on shelf to prevents stock-outs. Without Modus, the opportunity cost of a poor forecast, leading to poor orders could be devastating during a major promotion.
+            Accurate forecasts ensure there is enough product on shelf to prevent stock-outs. Without Modus, a poor forecast and inaccurate customer orders could be devastating, especially during a major promotion.
           </p>
 
-          <h2>9. Promote Wisely.</h2>
+          <h2>9. Promote wisely.</h2>
           <p>
-            Modus allows its user to map trade spend and track associated gross sales numbers, all in the same place. Track the effectiveness and make sure you never overshoot your promotion budget.
+            Modus allows its user to simultaneously map trade spend and track associated gross sales numbers. Track the effectiveness and make sure you never overshoot your promotion budget.
           </p>
           <p>
             Calculate the optimal outcomes based on user-defined promotion constraints. Maximize every dollar spent on trade based upon your brand’s unique circumstances.
           </p>
 
           <h2>
-            10. Manage your supply chain so you do not get taken advantage of.
+            10. Take control of your supply chain.
           </h2>
           <p>
             No one knows your demand better than you. Modus allows its users to compare distributor customer orders to internally generated forecasts to ensure your distributors are ordering correctly, and not over-indexing during an OI period.
           </p>
           <p>
-            Modus syncs with DC inventory data to help its users track their product down the supply chain. Oversee customer DC inventory days-on-hand and implement strategies to eliminate spoilage or stockouts downstream based on projected forecasts.
+            Modus syncs with DC inventory data to help its users track their product down the supply chain. Oversee customer DC inventory days-on-hand and implement strategies to eliminate downstream spoilage or stockouts based on projected forecasts.
           </p>
 
         </div>
